79c7547e06a6a10fce33767f47be5ab8a82c9d25
CodeSyntax{×_×} <rayudmakise@gmail.com>
2024-12-25 14:04:23 +0700

    Add files via upload

---
+++ Setup.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+echo   "attack Setup"
+read -sp "Press [ENTER] To Start."
+apt update -y
+apt upgrade -y
+apt install screen -y
+apt install htop -y
+apt install python3 -y
+apt install pip -y
+apt install nodejs -y
+apt install npm -y
+npm i request
+npm i node-fetch
+npm i p-limit
+npm i sync-request
+npm i cloudscraper
+npm i random-string
+npm i https-proxy-agent
+npm i axios
+npm i fake-useragent
+npm i random-ua
+npm i proxy-agent
+npm i request
+npm i puppeteer-extra
+npm i puppeteer
+npm i puppeteer-extra-plugin-stealth
+npm i requests
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i cloudflare-bypasser
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i random-useragent
+npm i set-cookie-parser
+npm i cloudflare-bypasser
+npm i axios
+npm i user-agents
+npm i cheerio
+npm i child_process
+npm i colors
+npm i crypto 
+npm i fake-useragent 
+npm i fingerprint-generator 
+npm i fingerprint-injector 
+npm i gradient-string 
+npm i header-generator 
+npm i http 
+npm i http2 
+npm i minimist 
+npm i playwright-extra 
+npm i zombie 
+npm i url 
+npm i superagent 
+npm i superagent-proxy 
+npm i ua-parser-js 
+npm i tls 
+npm i puppeteer-extra-plugin-recaptcha
+npm i requests
+npm i https-proxy-agent
+npm i crypto-random-string
+npm i events
+npm i fs
+npm i net
+npm i cloudscraper
+npm i request
+npm i hcaptcha-solver
+npm i randomstring
+npm i cluster
+npm i random-useragent
+npm i set-cookie-parser
+npm i cloudflare-bypasser
+npm i axios
+npm i user-agents
+npm i cheerio
+npm i child_process
+npm i colors
+npm i crypto
+npm i fake-useragent
+npm i fingerprint-generator
+npm i fingerprint-injector
+npm i gradient-string
+npm i header-generator
+npm i http
+npm i http2
+npm i minimist
+npm i playwright-extra
+npm i zombie
+npm i url
+npm i superagent
+npm i superagent-proxy
+npm i async
+npm i ua-parser-js
+npm i tls
+npm i puppeteer-extra-plugin-recaptcha
+npm i puppeteer-extra-plugin-adblocker
+npm i puppeteer-extra-plugin-stealth
+npm i hpack
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install ./google-chrome-stable_current_amd64.deb -y
+sudo apt -y install gcc g++ make
+npm i await-timeout
+npm i console-log-level
+npm i events
+npm i hpagent
+npm i got
+npm i crypto
+npm i cheerio
+npm i url
+npm i axios
+npm i https
+npm i request-curl
+npm i user-agents
+npm i uuid
+npm i tmp-promise
+npm i puppeteer-extra
+npm i puppeteer-extra-plugin-stealth
+npm i sleep-promise
+npm i puppeteer-extra-plugin-adblocker
+npm i puppeteer
+sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+sudo apt-get install -y libgbm-dev
